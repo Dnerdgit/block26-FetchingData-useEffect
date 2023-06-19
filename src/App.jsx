07205c0b0c469b81627f8b7ffb1fc160fd4512e0
DuBoris Smith <duboris.smith@hotmail.com>
2023-06-19 12:13:10 -0500

    Initial commit ContactList:  useEffect and fetch API

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useEffect, useState } from 'react'
+i
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        
+      </div>
+      <h1>Vite + React</h1>
+      <div>
+
+      </div>
+
+    </>
+  )
+}
+
+export default App
